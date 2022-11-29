9f8af774172e39cd5d64d8a6056b986b9d7f1b5c
wakeupmakeup <zerostar998@naver.com>
2022-11-29 18:59:07 +0900

    Initial Commit

---
+++ Bankey/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by 김태경 on 2022/11/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+
+   
+
+
+}
+
